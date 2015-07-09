2ade4a1b6da93883bdf9073b5805a4793f232c0e
Evan Hubinger <evanjhub@gmail.com>
2015-07-09 15:10:32 -0700

    Makes it a module

--- spyfall/main.py
+++ spyfall/main.py
@@ -8,6 +8,6 @@ parser.add_argument("ip", type=int, nargs=1)
 parser.add_argument("port", type=int, nargs=1)
 parser.add_argument("channel", type=str, nargs=1)
 
-if __name__ == "__main__":
+def main():
     args = parser.parse_args()
     server(args.ip, args.port, args.channel, LOCATION_FILE).run()
